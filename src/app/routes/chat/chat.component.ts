2c9fae4d54bfc4dd1ce9a33810e038e6bcaeb0a2
darioflo <darioflo19@icloud.com>
2025-04-02 22:54:20 -0600

    Visual

--- src/app/routes/chat/chat.component.ts
+++ src/app/routes/chat/chat.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
   imports: [],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
 export class ChatComponent {
+  constructor(private route: Router) {}
 
+  toLogin() {
+    this.route.navigate(['']);
+  }
 }
--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private router = inject(Router);
 
+  toChat() {
+    this.router.navigate(['/chat']);
+  }
 }
